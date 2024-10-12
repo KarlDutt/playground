b0e8d0c0fa395b76c9f4fddc49191de86368056a
KarlDutt <karlrasmusd@gmail.com>
2024-10-12 11:13:34 +0300

    loop works, but asks only for 1 thing

--- MadLibs.java
+++ MadLibs.java
@@ -1,19 +1,18 @@
 import java.util.Scanner;
 import java.util.ArrayList; // import the ArrayList class
 
-
 public class MadLibs {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> words = new ArrayList<String>();
 
-
-        for (int i = 0; i < 2; i++){
+        for (int i = 0; i < 2; i++) {
             System.out.println("Write a noun: ");
-            
+            String input1 = scanner.nextLine();
+            words.add(input1);
         }
+        System.out.println(words);
 
-        
     }
-    
+
 }

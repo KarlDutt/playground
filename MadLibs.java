17c05fb99d0cb97739adbf4b1267c42c377ba047
KarlDutt <karlrasmusd@gmail.com>
2024-10-12 11:03:58 +0300

    madlibs alpha

---
+++ MadLibs.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.ArrayList; // import the ArrayList class
+
+
+public class MadLibs {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> words = new ArrayList<String>();
+
+
+        for (int i = 0; i < 2; i++){
+            System.out.println("Write a noun: ");
+            
+        }
+
+        
+    }
+    
+}
